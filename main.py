d5e37a247f0fcf9451d930fb5d3a83ad6fabf357
garritypatwit <garrityp@wit.edu>
2022-12-13 14:18:34 -0500

    single character encryption and decryption

--- main.py
+++ main.py
@@ -12,8 +12,15 @@ ep = (4, 1, 2, 3, 2, 3, 4, 1)
 
 sw = (5, 6, 7, 8, 1, 2, 3, 4)
 
-s0 = [[1, 0, 3, 2], [3, 2, 1, 0], [0, 2, 1, 3], [3, 1, 3, 2]]
-s1 = [[0, 1, 2, 3], [2, 0, 1, 3], [3, 0, 1, 0], [2, 1, 0, 3]]
+s0 = [[1, 0, 3, 2],
+      [3, 2, 1, 0],
+      [0, 2, 1, 3],
+      [3, 1, 3, 2]]
+
+s1 = [[0, 1, 2, 3],
+      [2, 0, 1, 3],
+      [3, 0, 1, 0],
+      [2, 1, 0, 3]]
 
 k = 0b01101011
 
@@ -33,24 +40,17 @@ def binList(binVal, len):
 def keyGen(masterKey):
     # Create a list with the binary representation of the master key
     keyList = binList(masterKey, 10)
-    print(f"keyList = {keyList}")
     
     # Permute the key using the p10 function
     p10KeyList = applyPTable(keyList, p10)
     
-    print(f"p10KeyList = {p10KeyList}")
-    
     shift1 = leftShift(p10KeyList)
-    print(f"shift1 = {shift1}")
     
     shift2 = leftShift(shift1)
     shift2 = leftShift(shift2)
-    print(f"shift2 = {shift2}")
     
     k1 = applyPTable(shift1, p8)
     k2 = applyPTable(shift2, p8)
-    
-    print(f"{k1} + {k2}")
     
     return (k1, k2)
 
@@ -63,35 +63,78 @@ def applyPTable(input, pTable):
 def fk(input, key):
     p1 = input[:4]
     p2 = input[4:]
-    epTable = applyPTable(p2, ep)
+    epList = applyPTable(p2, ep)
     # xor with key
-    
-    # split 4 4
-    
+    for (index, val) in enumerate(epList):
+        epList[index] = val ^ key[index]
+    e1 = epList[:4]
+    e2 = epList[4:]
+
     # sbox
-    
+    e1 = applySBox(e1, s0)
+    e2 = applySBox(e2, s1)
     #combine + p4
+    resList = applyPTable(e1 + e2, p4)
     
-    return
+    for (index, val) in enumerate(resList):
+        resList[index] = val ^ p1[index]
+    return resList + p2
 
 def applySBox(input, sBox):
+    row = (input[0] << 1) + input[3]
+    col = (input[1] << 1) + input[2]
+    res = sBox[row][col]
+    return binList(res, 2)
 
-    return
-
-def encrypt():
+def encrypt(input, key):
+    (k1, k2) = keyGen(key)
     # convert text to list
-    binList
+    resList = binList(input, 8)
     # apply ip
-    ipList = applyPTable(binList, ip)
+    resList = applyPTable(resList, ip)
     # apply fk k1
-    fk()
+    resList = fk(resList, k1)
     # sw
-    
+    resList = applyPTable(resList, sw)
     # apply fk k2
-    
+    resList = fk(resList, k2)
     # ip -1
-    return
+    resList = applyPTable(resList, iip)
+    
+    res = ''.join(map(str, resList))
+    
+    return int(res, 2)
+
+def decrypt(input, key):
+    (k1, k2) = keyGen(key)
+    # convert text to list
+    resList = binList(input, 8)
+    # apply ip
+    resList = applyPTable(resList, ip)
+    # apply fk k1
+    resList = fk(resList, k2)
+    # sw
+    resList = applyPTable(resList, sw)
+    # apply fk k2
+    resList = fk(resList, k1)
+    # ip -1
+    resList = applyPTable(resList, iip)
+    
+    res = ''.join(map(str, resList))
+    
+    return int(res, 2)
 
 if __name__ == "__main__":
-    print(1^1)
-    (k1, k2) = keyGen(int("1010000010", 2))
+    # test = binList(0b10101001, 8)
+    # k1 = binList(0b10100100, 8)
+    # print(fk(test, k1))
+    input = 0b10101010 # 170
+    key = 0b0000000000
+    
+    ct = encrypt(input, key)
+    
+    print(f"cipher text = {format(ct, '#010b')}")
+    
+    pt = decrypt(ct, key)
+    
+    print(f"plain text = {format(pt, '#010b')}")
